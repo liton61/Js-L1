80f776d46745ff239aa43ce186ba34641e23a65e
liton61 <litonofficial61@gmail.com>
2025-03-17 14:49:36 +0600

    arithmetic operator added

---
+++ operator/arithmetic.js
@@ -0,0 +1,20 @@
+// arithmetic operators +, -, *, /, %
+
+var a = 10;
+var b = 20;
+
+var result = a + b;
+console.log(result);
+
+var result = a - b;
+console.log(result);
+
+var result = a * b;
+console.log(result);
+
+var result = a / b;
+console.log(result);
+
+var result = a % b;
+console.log(result);
+
