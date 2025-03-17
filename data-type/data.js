70d8381008b701fb2d6fa8fb8beefe0eba9932a2
liton61 <litonofficial61@gmail.com>
2025-03-17 14:10:23 +0600

    data type added

---
+++ data-type/data.js
@@ -0,0 +1,13 @@
+// data type
+
+var name = "Liton Mia"; // string
+var age = 29; // number
+var isMarried = true; // boolean
+var x = null; // null
+var y = undefined; // undefined
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof x);
+console.log(typeof y);
