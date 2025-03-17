7739615670ab1f37ab2bee0323e341d7f92000fd
liton61 <litonofficial61@gmail.com>
2025-03-17 15:00:34 +0600

    assignment operator added

---
+++ operator/assignment.js
@@ -0,0 +1,18 @@
+// assignment operator =, +=, -=, *=, /=, %=
+
+var a = 10;
+
+a += 5;
+console.log(a);
+
+a -= 5;
+console.log(a);
+
+a *= 5;
+console.log(a);
+
+a /= 5;
+console.log(a);
+
+a %= 5;
+console.log(a);
