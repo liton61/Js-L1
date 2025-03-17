000d43423ed5e7b871b0881b8fc3c309a31637a3
liton61 <litonofficial61@gmail.com>
2025-03-17 15:22:20 +0600

    logical operator added

---
+++ operator/logical.js
@@ -0,0 +1,13 @@
+// logical operator &&, ||, !
+
+var a = 10;
+var b = 20;
+
+var result = a > 5 && b > 15;
+console.log(result);
+
+var result = a > 5 || b > 15;
+console.log(result);
+
+var result = !(a > 5);
+console.log(result);
