775d2ded928040bb83d3dcf00d87550b61b22656
liton61 <litonofficial61@gmail.com>
2025-03-17 12:04:35 +0600

    variable added

---
+++ variable/variable.js
@@ -0,0 +1,10 @@
+// In javaScript we can declare variable in 3 ways using var, let, and const
+
+var designation = "Software Engineer"; // var: value can be change
+console.log(designation);
+
+let age = 29; // let: value can be change
+console.log(age);
+
+const myName = "Liton Mia"; // constant: value will not change
+console.log(myName);
